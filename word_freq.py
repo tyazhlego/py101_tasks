11d9c23c72133297d3a51b46c68c4f68edb35887
tyahlego <tyazhlego@gmail.com>
2020-11-07 13:14:03 +0300

    created a program for displaying the top 100 words without stops

--- word_freq.py
+++ word_freq.py
@@ -10,3 +10,18 @@
 
 if __name__ == '__main__':
     pass
+
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from collections import Counter
+import string
+
+def text_cleaner(path_to_file):
+    with open(path_to_file, "r") as b:
+        input_file = b.read()
+    noneed_char = stopwords.words('english') + list(string.punctuation)
+    need_char = [token for token in word_tokenize(input_file.lower()) if token not in noneed_char]
+    final_list = (Counter(need_char).most_common(100))
+    print(final_list)
+
+text_cleaner(input('Введите путь до файла:\n'))
